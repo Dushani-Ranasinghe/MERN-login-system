ddfa048a6b3c322f4c265bde550c8a06b6efd935
Dushani-Ranasinghe <uththaradushani@gmail.com>
2023-02-28 14:18:02 +0530

    created paths to template files

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  //paths to all template files
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
