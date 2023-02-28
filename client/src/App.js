f09f6d2cde105f139e127deffcb598e32e135863
Dushani-Ranasinghe <uththaradushani@gmail.com>
2023-02-28 15:42:08 +0530

    Added routes to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// import components
+import Username from "./components/Username";
+import Register from "./components/Register";
+import Password from "./components/Password";
+import Recovery from "./components/Recovery";
+import Profile from "./components/Profile";
+import Reset from "./components/Reset";
+import PageNotFound from "./components/PageNotFound";
+
+// root routes
+const router = createBrowserRouter([
+  { path: "/", element: <Username /> },
+  { path: "/register", element: <Register /> },
+  { path: "/password", element: <Password /> },
+  { path: "/recovery", element: <Recovery /> },
+  { path: "/profile", element: <Profile /> },
+  { path: "/reset", element: <Reset /> },
+  { path: "*", element: <PageNotFound /> },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <h1> MERN Login System</h1>
-    </div>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 }
 
